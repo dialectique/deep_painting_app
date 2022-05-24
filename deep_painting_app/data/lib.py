ae0ca36d5d69b6cf846394598eae5261b448957e
Gilles Chardon <gilles.chardon.fr@gmail.com>
2022-05-25 02:06:18 +0900

    creation of lib.py which will contains preprocessing functions

---
+++ deep_painting_app/data/lib.py
@@ -0,0 +1,11 @@
+import os
+import math
+import numpy as np
+import pandas as pd
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+
+
+
+if __name__ == 'main':
+    print("...")

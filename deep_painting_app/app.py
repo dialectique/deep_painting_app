d655a4e651668a4edc436054068e3ee1fb865154
Kevser Durak <kevser_durak@icloud.com>
2022-05-30 15:01:27 +1000

    fixed the guessing game

--- deep_painting_app/app.py
+++ deep_painting_app/app.py
@@ -1,10 +1,12 @@
 #Import Libraries
+from re import sub
 import streamlit as st
 from PIL import Image
 import matplotlib.pyplot as plt
 import os
 from utils import head, set_bg, equal_text
 from deep_painting_app.explore_data import random_painting
+
 
 #Opens and displays the image
 def get_opened_image(image):
@@ -14,17 +16,11 @@ def get_opened_image(image):
 head()
 
 #Sets background image
-set_bg('data/background.jpg')
+set_bg('data/black_background.jpg')
 
 #Uploads the image file
 image_file = st.file_uploader('Upload an image', type = ['png', 'jpg',
                                                          'jpeg', 'pdf'])
-# #Camera Input
-# picture = st.camera_input("Take a picture")
-# if picture and st.button('Load'):
-#     pic = get_opened_image(picture)
-#     with st.expander("Selected Image", expanded = True):
-#         st.image(pic, use_column_width = True)
 
 #Displays the image
 if image_file and st.button('Load'):
@@ -38,41 +34,39 @@ if image_file and st.button('Load'):
 # st.markdown(f'This image belongs to **{pred}** class with .. probability')
 
 #THE GUESSING GAME
+path = 'data/orgImg'
+
+if 'random_image' not in st.session_state:
+    st.session_state['random_image'] = random_painting(path)
+    print("Init")
+
+def form2_callback():
+    st.session_state['random_image'] = random_painting(path)
+    print("callback")
+
 with st.sidebar:
     st.title('Guess the Movement')
-    path = 'data/orgImg'
-    random_image = random_painting(path, img_height=180, img_width=180)
     fig, ax = plt.subplots()
-    ax.imshow(random_image[0]/255)
+    ax.imshow(st.session_state['random_image'][0]/255)
     ax.set_axis_off()
     st.pyplot(fig)
 
-    # movement = st.radio("What do you think?",
-    #                 ('High Renaiss', 'Impress', 'Northern Renaiss',
-    #                  'Post Impress', 'Rococo', 'Ukiyo'))
+    label = st.session_state['random_image'][1]
+    label = equal_text(label)
 
-    # movement = st.selectbox("What do you think?",
-    #                 ('High Renaiss', 'Impress', 'Northern Renaiss',
-    #                  'Post Impress', 'Rococo', 'Ukiyo'))
 
     with st.form(key ='Form1'):
         movement = st.radio("What do you think?",
-                    ('Empty', 'High Renaiss', 'Impress', 'Northern Renaiss',
-                     'Post Impress', 'Rococo', 'Ukiyo'), on_change=None)
+                    ('High Renaiss', 'Impress', 'Northern Renaiss',
+                     'Post Impress', 'Rococo', 'Ukiyo'))
 
         submitted = st.form_submit_button(label = 'Submit')
 
-    label = random_image[1]
-    label = equal_text(label)
+    if submitted:
+        if movement == label:
+            st.write('Well done!')
+        else:
+            st.write(f'Ooops! It was {label} .')
 
-    if movement == label:
-        st.write('Well done!')
-    else:
-        st.write(f'Ooops! It was {label} .')
-
-    # placeholder = st.empty()
-    # with placeholder.container():
-    #     btn = st.button("Next")
-
-    # if btn:
-    #     placeholder.empty()
+        #with st.form(key = 'Form2'):
+        btn = st.button("Next", on_click= form2_callback)
